5a16660875c7db3ca94275ec541783d308c87030
Samad126 <samed192005@gmail.com>
2024-08-12 22:13:24 +0400

    init commit

---
+++ data.js
@@ -0,0 +1,83 @@
+const products = [
+    {
+        "image": {
+            "thumbnail": "/images/image-waffle-thumbnail.jpg",
+            "orginal": "/images/image-waffle-desktop.jpg"
+        },
+        "name": "Waffle with Berries",
+        "category": "Waffle",
+        "price": 6.50
+    },
+    {
+        "image": {
+            "thumbnail": "/images/image-creme-brulee-thumbnail.jpg",
+            "orginal": "/images/image-creme-brulee-desktop.jpg"
+        },
+        "name": "Vanilla Bean Crème Brûlée",
+        "category": "Crème Brûlée",
+        "price": 7.00
+    },
+    {
+        "image": {
+            "thumbnail": "/images/image-macaron-thumbnail.jpg",
+            "orginal": "/images/image-macaron-desktop.jpg"
+        },
+        "name": "Macaron Mix of Five",
+        "category": "Macaron",
+        "price": 8.00
+    },
+    {
+        "image": {
+            "thumbnail": "/images/image-tiramisu-thumbnail.jpg",
+            "orginal": "/images/image-tiramisu-desktop.jpg"
+        },
+        "name": "Classic Tiramisu",
+        "category": "Tiramisu",
+        "price": 5.50
+    },
+    {
+        "image": {
+            "thumbnail": "/images/image-baklava-thumbnail.jpg",
+            "orginal": "/images/image-baklava-desktop.jpg"
+        },
+        "name": "Pistachio Baklava",
+        "category": "Baklava",
+        "price": 4.00
+    },
+    {
+        "image": {
+            "thumbnail": "/images/image-meringue-thumbnail.jpg",
+            "orginal": "/images/image-meringue-desktop.jpg"
+        },
+        "name": "Lemon Meringue Pie",
+        "category": "Pie",
+        "price": 5.00
+    },
+    {
+        "image": {
+            "thumbnail": "/images/image-cake-thumbnail.jpg",
+            "orginal": "/images/image-cake-desktop.jpg"
+        },
+        "name": "Red Velvet Cake",
+        "category": "Cake",
+        "price": 4.50
+    },
+    {
+        "image": {
+            "thumbnail": "/images/image-brownie-thumbnail.jpg",
+            "orginal": "/images/image-brownie-desktop.jpg"
+        },
+        "name": "Salted Caramel Brownie",
+        "category": "Brownie",
+        "price": 4.50
+    },
+    {
+        "image": {
+            "thumbnail": "/images/image-panna-cotta-thumbnail.jpg",
+            "orginal": "/images/image-panna-cotta-desktop.jpg"
+        },
+        "name": "Vanilla Panna Cotta",
+        "category": "Panna Cotta",
+        "price": 6.50
+    }
+]
---
+++ meniOxu.md
@@ -0,0 +1,16 @@
+Sayt tam funksiyanal, responsiv və dizaynla birə-bir olmalıdır.
+Saytın urli -  https://product-list-with-cart-fm.netlify.app
+
+1.  Desertləri səbətə atmaq funskiyası var.
+
+2.  Əgər səbətdə həmin item varsa, add to cart sözünün yerində counter çıxmalıdır. Artırıb azaltdıqca sağ tərəfdəki səbətdə də dəyişməlidir.
+
+3. Səbətdən siləndə də cartın alt hissəsində çıxan counter getməli yerinə add to cart sözü gəlməlidir.
+
+4) Yekunda Confirm Orderi kliklədikdə isə bizim yekun sifarişimizi göstərən bir pəncərə açılmalıdı.
+
+5) Açılan Order Confirm pəncərəsində balaca şəkillər datadakı thumbnail propertysindəki olan şəkillərdir. Normal olanlar ise orginal olanlardır.
+
+6) Submit new orderi basdıqda isə səbətimiz sıfırlanmalıdır.
+
+Qeyd - Dediyim şeylərin hamısı screenshots papkasındakı şəkillərdə əks olunub. Sizə lazım olan şəkillər və data zipin içərisindədir. Uğurlar 😉
